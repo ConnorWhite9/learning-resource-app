b601fcdecb5d5f7d538cc31b57e7d6c6524712fd
Connor <connorlwhite@icloud.com>
2024-08-18 11:49:26 -0400

    Created crud folder and added get_user function

--- server/services/user.py
+++ server/services/user.py
@@ -1,9 +1,1 @@
-#Database interactions regarding the user table
 
-from sqlalchemy.orm import Session
-
-from . import models, schemas
-
-
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
