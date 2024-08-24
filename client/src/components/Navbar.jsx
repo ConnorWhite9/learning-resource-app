e4420808e519a494af004ccd97d061ae3d87758d
Daniel Wu <danielw72908@gmail.com>
2024-08-24 11:38:51 -0400

    Created a navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function Navbar() {
+  const location = useLocation();
+  return (
+    <div className="w-screen flex justify-center items-center h-16">
+      <ul className="flex list-none p-0 m-0 space-x-8">
+        <li>
+          <Link
+            to="/"
+            className={`relative font-custom text-2xl transition-all duration-300`}
+          >
+            Home
+            <span
+              className={`absolute left-0 right-0 bottom-0 h-0.5 bg-black transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 ${
+                location.pathname === "/" ? "scale-x-100" : ""
+              }`}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/courses"
+            className={`relative font-custom text-2xl transition-all duration-300`}
+          >
+            Courses
+            <span
+              className={`absolute left-0 right-0 bottom-0 h-0.5 bg-black transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 ${
+                location.pathname === "/courses" ? "scale-x-100" : ""
+              }`}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/aboutus"
+            className={`relative font-custom text-2xl transition-all duration-300`}
+          >
+            About Us
+            <span
+              className={`absolute left-0 right-0 bottom-0 h-0.5 bg-black transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 ${
+                location.pathname === "/aboutus" ? "scale-x-100" : ""
+              }`}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/register"
+            className={`relative font-custom text-2xl transition-all duration-300`}
+          >
+            Get Started
+            <span
+              className={`absolute left-0 right-0 bottom-0 h-0.5 bg-black transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 ${
+                location.pathname === "/register" ? "scale-x-100" : ""
+              }`}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/login"
+            className={`relative font-custom text-2xl transition-all duration-300`}
+          >
+            Login
+            <span
+              className={`absolute left-0 right-0 bottom-0 h-0.5 bg-black transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 ${
+                location.pathname === "/login" ? "scale-x-100" : ""
+              }`}
+            />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LandingPage() {
+  return <div>test</div>;
+}
+
+export default LandingPage;
