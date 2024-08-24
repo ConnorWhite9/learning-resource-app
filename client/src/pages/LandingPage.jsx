a04e1290840cbc4a3490812c0cda1968ae8be07a
Daniel Wu <danielw72908@gmail.com>
2024-08-24 12:17:53 -0400

    Created the features section of the landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Features from "../components/Features";
 
 function LandingPage() {
   return (
     <div className="bg-black">
       <Hero />
+      <Features />
     </div>
   );
 }
