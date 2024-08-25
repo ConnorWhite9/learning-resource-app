442d31cec66d42a90ef900507eab146033eb4d4a
Connor <connorlwhite@icloud.com>
2024-08-25 18:16:15 -0400

    Created get_courses function

--- server/crud/course.py
+++ server/crud/course.py
@@ -1,3 +1,11 @@
 from sqlalchemy.orm import Session
 
 from . import models, schemas
+
+
+def get_courses(db:Session):
+    try:
+        courses = db.query(models.Course).all()
+        return courses
+    except:
+        return ValueError
