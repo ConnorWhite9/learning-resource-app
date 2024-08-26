322e94ea5bfc4d2fad155f1e7a65903df3e99732
Daniel Wu <danielw72908@gmail.com>
2024-08-25 21:28:14 -0400

    Created the content section of the landing page

---
+++ client/src/components/Content.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Content() {
+  return (
+    <div className="flex justify-center items-center h-[500px]">
+      <div className="flex justify-center items-center w-[90%]">
+        <section className="text-white flex-1 flex flex-col justify-center items-center">
+          <div>
+            <h3 className="font-bold text-2xl mb-3">Stay on Task</h3>
+            <p className="mb-2">
+              These lessons will cut the unnecessary fluff and get straight to
+              the point.
+            </p>
+            <ul>
+              <li className="list-disc ml-[1em] mb-1">
+                Pacing catered towards the individuals{" "}
+              </li>
+              <li className="list-disc ml-[1em] mb-1">
+                Quizzes to ensure mastery
+              </li>
+              <li className="list-disc ml-[1em] mb-1">
+                Mastery bar to visually represent understanding
+              </li>
+            </ul>
+          </div>
+        </section>
+        <aside className="flex justify-center items-center flex-1">
+          <img src="./hero.png" alt="placeholder" className="w-[60%]" />
+        </aside>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
+import Content from "../components/Content";
 
 function LandingPage() {
   return (
     <div className="bg-black">
       <Hero />
       <Features />
+      <Content />
     </div>
   );
 }
