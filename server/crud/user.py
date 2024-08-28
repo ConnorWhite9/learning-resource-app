56bc9f8443e78540a708a017cb792a9be3bb633a
Connor <connorlwhite@icloud.com>
2024-08-28 16:42:17 -0400

    Preliminary model for create_user function in crud

--- server/crud/user.py
+++ server/crud/user.py
@@ -11,3 +11,25 @@ async def get_user(db: Session, user_id: int):
         return user
     except:
         return ValueError
+    
+
+async def login(db: Session, email: str, password: str):
+    try:
+        username = await db.query(models.User).filter(models.User.email == email).first()
+        password = await db.query(models.User).filter(models.User.password == hash).first()
+        if username or password is None:
+            return False
+        else:
+            return True
+    except:
+        return ValueError
+    
+
+
+async def create_user(db: Session, create_user_request: CreateUserRequest):
+    create_user_model = models.User(
+        username=create_user_request.username,
+        hashed_password = bcrypt_context.hash(create_user_request.password),
+    )
+    db.add(create_user_model)
+    db.commit()
--- server/services/auth.py
+++ server/services/auth.py
@@ -4,10 +4,25 @@ from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from typing import Annotated
 from fastapi import Depends
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from server.db.database import get_db
 
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
+SECRET_KEY = 'fjfjfjfjf'
+ALGORITHM = 'HS256'
+
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
