f6db33a91a25030bf4c789bce200af6095a30a17
Daniel Wu <danielw72908@gmail.com>
2024-09-03 12:06:09 -0400

    Created all the lessons

--- client/src/components/Lesson.jsx
+++ client/src/components/Lesson.jsx
@@ -6,18 +6,21 @@ import Documentation from "./Documentation";
 
 
 
-function Lesson({ quizzes, documentation, title, color }) {
+function Lesson({ quizzes, documentation, title, color, video }) {
   return (
     <>
       <div className="flex justify-center mt-[40px] font-sans">
-        <div className={`flex w-[70%] h-[701px] bg-[${color}] rounded-lg`}>
+        <div
+          className="flex w-[70%] h-[701px] rounded-lg"
+          style={{ backgroundColor: color }}
+        >
           <div className="flex">
             <div className="flex flex-col justify-between w-[30%] h-full">
               <Overview title={title} />
               <Quizzes quizzes={quizzes} />
             </div>
             <div className="flex flex-col">
-              <Video link="https://www.youtube.com/embed/HD13eq_Pmp8" />
+              <Video link={video} />
               <Documentation links={documentation} />
             </div>
           </div>
--- client/src/components/Lessons.jsx
+++ client/src/components/Lessons.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Lesson from './Lesson'
+import React from "react";
+import Lesson from "./Lesson";
 
-const quizzes = [
+const htmlQuizzes = [
   "Introduction to HTML",
   "HTML Document Standards",
   "Hyperlinks",
@@ -13,18 +13,74 @@ const quizzes = [
   "Buttons",
   "Forms",
 ];
+const cssQuizzes = [
+  "Introduction to CSS",
+  "In-line, Internal, and External CSS",
+  "Box Model",
+  "Fonts",
+  "Borders",
+  "Background",
+  "Margins",
+  "Position",
+  "Pseudo Classes",
+  "Shadows",
+  "Icons",
+];
+const pythonQuizzes = [
+  "Introduction to Python",
+  "Variables and Data Types",
+  "Output/Printing",
+  "User Input",
+  "Lists",
+  "Loops",
+  "Functions",
+  "Classes",
+  "String Methods",
+];
 
-const documentation = [
+const htmlDocumentation = [
   "https://www.w3schools.com/html/",
   "https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML",
 ];
+const cssDocumentation = [
+  "https://www.w3schools.com/css/",
+  "https://developer.mozilla.org/en-US/docs/Learn/CSS",
+];
+const pythonDocumentation = [
+  "  https://www.w3schools.com/python/",
+  "https://www.geeksforgeeks.org/python-programming-language-tutorial/",
+];
+
+const htmlVideo = "https://www.youtube.com/embed/HD13eq_Pmp8";
+const cssVideo = "https://www.youtube.com/embed/wRNinF7YQqQ";
+const pythonVideo = "https://www.youtube.com/embed/VchuKL44s6E"
 
 function Lessons() {
   return (
     <div>
-      <Lesson quizzes={quizzes} documentation={documentation} title="Introduction to HTML" color="#B2DF8A"/>
+      <Lesson
+        quizzes={htmlQuizzes}
+        documentation={htmlDocumentation}
+        title="Introduction to HTML"
+        color="#B2DF8A"
+        video={htmlVideo}
+      />
+      <Lesson
+        quizzes={cssQuizzes}
+        documentation={cssDocumentation}
+        title="Introduction to CSS"
+        color="#FFA76C"
+        video={cssVideo}
+      />
+      <Lesson
+        quizzes={pythonQuizzes}
+        documentation={pythonDocumentation}
+        title="Introduction to Python"
+        color="#A6DBFF"
+        video={pythonVideo}
+      />
     </div>
-  )
+  );
 }
 
-export default Lessons
+export default Lessons;
