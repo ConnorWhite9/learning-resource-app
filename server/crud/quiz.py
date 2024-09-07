8e9aef6e436cb1f8ac76405099efff28f305b3fc
Connor <connorlwhite@icloud.com>
2024-09-06 22:57:14 -0400

    Created get_course_quizs function

--- server/crud/quiz.py
+++ server/crud/quiz.py
@@ -2,16 +2,23 @@
 from sqlalchemy.orm import Session
 
 from . import models, schemas
+from models.quiz import *
+from models.course import *
 
 
 def get_quiz(db: Session, level: int, course: str ):
     try: 
-        quiz = db.query(models.Quiz).filter(
-            models.Quiz.level == level,
-            models.Quiz.course == course
+        quiz = db.query(Quiz).filter(
+            Quiz == level,
+            Quiz == course
         ).first()
         return quiz
     except:
         return ValueError
 
-
+def get_course_quizs(db: Session, course: str):
+    try: 
+        quizs = db.query(Quiz).filter(Quiz.course == course)
+        return quizs
+    except:
+        raise ValueError
