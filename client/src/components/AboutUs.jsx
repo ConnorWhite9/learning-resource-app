ea348284c0baf4c7a60fb8ce7bca4e82cdf4b6d8
Connor <connorlwhite@icloud.com>
2024-09-17 08:21:19 -0400

    Created the aboutus component and completed aboutUs page for now.

---
+++ client/src/components/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Aboutus() {
+    return(
+        <div>
+            <p className="text-white w-[20rem] ml-auto mr-auto text-center mt-[8rem] text-[1.3rem] " >Welcome to IntelliProgramming. We are website made by two developers looking to help bring new people into the world of programming. We hope you can find the same
+                passion we have for programming on this very website. So go take a look aroung. Take some courses and hopefully you can take something amazing away from this experience.
+            </p>
+        </div>
+    );
+}
+
+
+export default Aboutus;
