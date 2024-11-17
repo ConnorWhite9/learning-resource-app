a91398c162438f462422b3089193ae839edb4a82
Connor <connorlwhite@icloud.com>
2024-11-16 21:14:47 -0500

    Set up button for demo user and mock function

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import {Link} from "react-router-dom";
 function Hero() {
+
+  const demoLogin = () => {
+    const demoUser = {
+      id: `demo_${Date.now()}`,
+      name: "Demo User",
+      quizScores: [], // or whatever initial data you need
+    };
+    localStorage.setItem('demoUser', JSON.stringify(demoUser));
+    
+  }
+
+
   return (
     <div className="flex font-custom h-[500px] mb-8 justify-center items-center">
       <div className="flex justify-center items-center w-[90%]">
@@ -13,11 +25,18 @@ function Hero() {
               Greater mastery and understanding on programming languages—HTML,
               CSS, Python.
             </p>
-            <Link to="/register">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-[125px] mt-[40px]">
-                Sign Up
-              </button>
-            </Link>
+            <div className="flex flex-row">
+              <Link to="/register">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-[125px] mt-[40px]">
+                  Sign Up
+                </button>
+              </Link>
+              <Link to="/" className="ml-[2rem]">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white text-nowrap font-bold py-2 px-4 rounded w-[125px] mt-[40px]" >
+                  Demo User
+                </button>
+              </Link>
+            </div>
           </div>
         </section>
         <aside className="flex justify-center items-center flex-1">
