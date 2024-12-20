95968c5d062736b8238bf06627bc643a47b69f7a
Connor <connorlwhite@icloud.com>
2024-12-19 22:16:46 -0500

    Made courses into one singular column on mobile devices

--- client/src/components/Lesson.jsx
+++ client/src/components/Lesson.jsx
@@ -12,7 +12,7 @@ function Lesson({ language, quizzes, documentation, title, color, video, userInf
           className="flex flex-col md:flex-row w-[70%] h-auto md:h-[40rem] rounded-lg"
           style={{ backgroundColor: color }}
         >
-          <div className="flex flex-col md:w-[30%] w-full h-auto md:h-full">
+          <div className="flex flex-col md:w-[30%] w-full h-auto md:h-full" >
             <Overview language={language} title={title} userInfo={userInfo} />
             <Quizzes language={language} quizzes={quizzes} userInfo={userInfo} />
           </div>
