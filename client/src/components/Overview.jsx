4ca4b50da44c84327d2db7afdd375cea350baa00
Connor <connorlwhite@icloud.com>
2025-01-21 00:42:04 -0500

    Redid margin-left to look good on laptop for now

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -22,12 +22,11 @@ function Overview({ language, title, userInfo }) {
 
   return (
     <div className="flex flex-col justify-center items-center border-b-2 border-r-2 border-black w-full h-full">
-      <p className="font-semibold text-2xl mt-2">{title}</p>
+      <p className="font-semibold text-xl mt-2">{title}</p>
       <div className="flex justify-between items-center w-[90%] mt-4">
-        <div className="flex flex-col p-4 max-md:ml-[0rem] ml-[0.5vw]">
-          <h2>Mastery:   {masteryNumber}% </h2>
-          
-           <ProgressBar progress={masteryNumber} />
+        <div className="flex flex-col p-2 ml-[1vw] ">
+          <h2 className="font-semibold">Mastery:   {masteryNumber}% </h2>  
+          <ProgressBar progress={masteryNumber} />
             
         </div>
         <div className="flex flex-col p-4 mr-[2rem] ">
